86aeb872a1752cc5ac47da525afea3af36ec27bd
Mordecai Yiadom <mordeyiadom@gmail.com>
2026-04-19 17:54:44 -0400

    Init Repo

---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.finalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button playButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //Init Homepage activity UI members
+        initPlayButton();
+    }
+
+    private void initPlayButton()
+    {
+        playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener((view)-> {
+            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            startActivity(intent);});
+    }
+
+
+
+
+}
+
